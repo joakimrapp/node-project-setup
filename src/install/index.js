5f17e53e26abda1a59e3a2e68fce40336c439a13
Joakim J. Rapp <joakimrapp@hotmail.com>
2017-05-23 16:12:32 +0200

    wip

--- src/install/index.js
+++ src/install/index.js
@@ -1,2 +1,8 @@
 const path = require( 'path' );
 const fs = require( 'fs' );
+const packagjsonepath = path.resolve( __dirname, '../../../../../package.json' );
+if( fs.existsSync( packagjsonepath ) ) {
+	const packagejson = require( packagjsonepath );
+	Object.assign( packagejson.scripts, require( '../../config/package.json' ).scripts );
+	fs.writeFileSync( packagjsonepath, JSON.stringify( packagejson, undefined, '\t' ), 'utf-8' );
+}
